e749df20b92332e3d33970afe18e9ca80db7475c
liruimin <m18210870260@163.com>
2019-01-08 19:09:00 +0800

    'less配置新增'

--- src/pages/Life.js
+++ src/pages/Life.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Child from './Child';
-
+import './index.less';
+import { Button } from "antd";
+// import 'antd/dist/antd.css';
 export default class Life extends React.Component{
   constructor(props){
     super(props);
@@ -14,12 +16,10 @@ export default class Life extends React.Component{
     })
   }
   render(){
-    let style ={
-      padding: 30
-    }
-    return <div style = {style}>
+   
+    return <div className="content">
       <p>react生命周期介绍</p>
-      <button onClick={this.handleClick.bind(this)}>点击一下</button>
+      <Button onClick={this.handleClick.bind(this)}>点击一下</Button>
       <p>{this.state.count}</p>
       <Child names={this.state.count}></Child>
     </div>
